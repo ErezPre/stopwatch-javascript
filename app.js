a490d63f9f3e9c5b330a34e48ae76737817edb30
Erez Prepelitzki <prepelitzki@gmail.com>
2022-03-27 16:43:03 +0300

    1

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let counter = 0;
 
 let running = false;
+const hours = document.querySelector(`.hours`);
+const minutes = document.querySelector(`.minutes`);
+const seconds = document.querySelector(`.seconds`);
+const centiseconds = document.querySelector(`.centiseconds`);
 const start = document.querySelector(`.start`);
 const reset = document.querySelector(`.reset`);
 const stoppingList = document.querySelector(`.stopping-list`);
-const time = document.querySelector(`.time`);
-time.textContent = counter;
+const timeContainer = document.querySelector(`.time-container`);
+// time.textContent = counter;
 
 start.addEventListener("click", startTime);
 reset.addEventListener("click", resetTime);
@@ -14,23 +18,28 @@ function startTime() {
   if (running) {
     running = false;
     start.textContent = `resume`;
-    stoppingList.innerHTML = `<div>${counter}</div> ${stoppingList.innerHTML}`;
+    start.style.color = "rgb(30, 226, 30)";
+
+    stoppingList.innerHTML = `<div>${hours.textContent}:${minutes.textContent}:${seconds.textContent}.${centiseconds.textContent}</div> ${stoppingList.innerHTML}`;
   } else {
     running = true;
     start.textContent = `stop`;
+    start.style.color = "red";
   }
 }
+
 function updateTime() {
   if (running) {
     counter += 10;
-    time.textContent = formatTime(counter);
+    setTime();
   }
 }
 function resetTime() {
   running = false;
   start.textContent = `start`;
+  start.style.color = "rgb(30, 226, 30)";
   counter = 0;
-  time.textContent = counter;
+  setTime();
   stoppingList.innerHTML = ``;
 }
 function twoDigitNumString(num) {
@@ -42,16 +51,26 @@ function twoDigitNumString(num) {
 const hoursInMs = 60 * 60 * 1000;
 const minutesInMs = 60 * 1000;
 const secondsInMs = 1000;
-const centisecondsInMs = 100;
+const centisecondsInMs = 10;
 
-function formatTime() {
-  const hours = Math.floor(counter / hoursInMs);
-  const minutes = Math.floor(counter / minutesInMs);
-  const seconds = Math.floor(counter / secondsInMs);
-  const centiseconds = Math.floor((counter / centisecondsInMs) % 100) / 10;
+function setTime() {
+  // counter = minutesInMs * 88000 + 757;
 
-  return `${hours}:${twoDigitNumString(minutes)}:${twoDigitNumString(seconds)}`;
+  hours.textContent = twoDigitNumString(Math.floor(counter / hoursInMs));
+
+  minutes.textContent = twoDigitNumString(
+    Math.floor(counter / minutesInMs) % 60
+  );
+
+  seconds.textContent = twoDigitNumString(
+    Math.floor(counter / secondsInMs) % 60
+  );
+
+  centiseconds.textContent = twoDigitNumString(
+    Math.floor(counter / centisecondsInMs) % 100
+  );
 }
+// formatTime(
 
 counter = setInterval(updateTime, 10);
 console.log(formatTime(2));
